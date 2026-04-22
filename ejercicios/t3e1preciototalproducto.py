bd99075b411419a49d443cff4d67959c29702b74
Maria Tapia <mariatm89@yahoo.es>
2026-04-22 20:48:32 +0200

    Update t3e1preciototalproducto.py

--- ejercicios/t3e1preciototalproducto.py
+++ ejercicios/t3e1preciototalproducto.py
@@ -14,7 +14,8 @@ def total_compra(precio_unidad,cantidad,descuento,Iva):
     iva_total = precio_con_descuento * (Iva / 100)
     precio_final = precio_con_descuento + iva_total
     return precio_final
+    print("Introduce los datos de la compra:")
+    nombre_producto, precio_unidad, cantidad, descuento, Iva = datos_compra()
+    print("Total: ", total_compra(precio_unidad,cantidad,descuento,Iva))
 
-print("Introduce los datos de la compra:")
-nombre_producto, precio_unidad, cantidad, descuento, Iva = datos_compra()
-print("Total: ", total_compra(precio_unidad,cantidad,descuento,Iva))    
+total_compra()        
